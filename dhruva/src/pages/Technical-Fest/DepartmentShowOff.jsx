d6402b21c6f4c397854dd9309e63761f824f31f5
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-04-16 10:24:52 +0530

    fix : fetch data

--- dhruva/src/pages/Technical-Fest/DepartmentShowOff.jsx
+++ dhruva/src/pages/Technical-Fest/DepartmentShowOff.jsx
@@ -4,21 +4,25 @@ import axios from "axios";
 import SkeletonCard from "../../components/page-components/SkeletonCard.jsx";
 
 function DepartmentShowOff() {
-  const [department, setDepartment] = useState([]);
-  const [loading, setLoading] = useState(true); 
+  const [departments, setDepartments] = useState([]); // State to hold filtered departments
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchDepartment = async () => {
       try {
         const api = import.meta.env.VITE_APP_API;
-        const response = await axios.get(`${api}api/get-events`);
-        setDepartment(response.data);
+        const res = await axios.get(`${api}api/get-events`);
+        const filteredDepartments = res.data.filter(
+          (dept) => dept.events.length !== 0
+        );
+        setDepartments(filteredDepartments);
       } catch (error) {
         console.log(error.message);
       } finally {
-        setLoading(false); 
+        setLoading(false);
       }
     };
+
     fetchDepartment();
   }, []);
 
@@ -27,7 +31,7 @@ function DepartmentShowOff() {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-6 max-w-[1440px]">
         {loading
           ? Array.from({ length: 4 }).map((_, i) => <SkeletonCard key={i} />)
-          : department.map((dept, index) => (
+          : departments.map((dept, index) => (
               <DepartmentCard events={dept} key={index} />
             ))}
       </div>
